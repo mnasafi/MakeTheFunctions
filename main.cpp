0a712fa0cfe8292885ce0fce4b13ed3771a4b557
Mustafa Nasafi <mnasafi@users.noreply.github.com>
2018-04-19 23:41:57 -0700

    Update main.cpp
    Finished version

--- main.cpp
+++ main.cpp
@@ -4,41 +4,70 @@
 using namespace std;
 
 //prototypes:
-
+void printVector(const vector<int> &);
+bool isPositive(int &);
+int fillWith( vector<int> &, int);
 
 /////DO NOT TOUCH/////
 int main()
 {
-  vector <int> projects;
-  vector <int> quizzes;
+        vector <int> projects;
+        vector <int> quizzes;
 
-  int numProjects;
-  int numQuizzes;
+        int numProjects;
+        int numQuizzes;
 
-  cout<<"\nHow many projects are there? ";
-  cin>>numProjects;
-  cout<<"\nHow many quizzes are there? ";
-  cin>>numQuizzes;
+        cout<<"\nHow many projects are there? ";
+        cin>>numProjects;
+        cout<<"\nHow many quizzes are there? ";
+        cin>>numQuizzes;
 
-  //only proceed if both numbers are positive
-  if( isPositive(numProjects) && isPositive(numQuizzes) )
-  {
-    projects.resize(numProjects);
-    quizzes.resize(numQuizzes);
+        //only proceed if both numbers are positive
+        if( isPositive(numProjects) && isPositive(numQuizzes) )
+        {
+                projects.resize(numProjects);
+                quizzes.resize(numQuizzes);
 
-    fillWith(projects, 90); // Fills the vector with all value 90s
-    fillWith(quizzes, 80); // Fills the vector with all value 80s
+                fillWith(projects, 90); // Fills the vector with all value 90s
+                fillWith(quizzes, 80); // Fills the vector with all value 80s
 
-    cout<<"\nProject Scores:\n";
-    printVector(projects); // Prints vector with space after each cell
-    cout<<"\n\nQuiz Scores:\n";
-    printVector(quizzes); // Prints vector with space after each cell
-  }
-  else
-  {
-    cout<<"\nSorry, numbers must be bigger than 0.\n";
-  }
-  return 0;
+                cout<<"\nProject Scores:\n";
+                printVector(projects); // Prints vector with space after each cell
+                cout<<"\n\nQuiz Scores:\n";
+                printVector(quizzes); // Prints vector with space after each cell
+        }
+        else
+        {
+                cout<<"\nSorry, numbers must be bigger than 0.\n";
+        }
+        return 0;
 }
 
 //function definitions
+void printVector(const vector<int> &space)
+{
+        for(int q = 0; q < space.size(); q++)
+        {
+                cout<<space[q]<<" ";
+        }
+}
+
+bool isPositive(int &positive)
+{
+        if(positive > 0)
+        {
+                return true;
+        }
+        else
+        {
+                return false;
+        }
+}
+
+int fillWith( vector<int> &list, int nums)
+{
+        for(int n = 0; n < list.size(); n++)
+        {
+                list[n] = nums;
+        }
+}
